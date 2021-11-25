034e3e85387fc93bac9f25a3daffab9b4cbc6bd6
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-25 10:30:24 +0100

    add html structure to App.vue

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="site_header"></div>
+</template>
+
+<script>
+export default {
+  props: {},
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+</style>
