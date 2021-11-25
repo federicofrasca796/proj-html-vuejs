dd81d49da4ba14ecee8ac4c87000c200a83e2255
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-25 22:24:51 +0100

    start sitemain | add section 1 (to fix vertical bar)

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,5 +1,26 @@
 <template>
-  <div id="site_main"></div>
+  <div id="site_main">
+    <section class="results_overview container">
+      <div class="row">
+        <div class="col-3 accomplishment">
+          <div class="numbers">1.926</div>
+          <div class="text-uppercase txt">finished session</div>
+        </div>
+        <div class="col-3 accomplishment">
+          <div class="numbers">100%</div>
+          <div class="text-uppercase txt">satisfaction rate</div>
+        </div>
+        <div class="col-3 accomplishment">
+          <div class="numbers">3092+</div>
+          <div class="text-uppercase txt">enrolled learners</div>
+        </div>
+        <div class="col-3 accomplishment">
+          <div class="numbers">20</div>
+          <div class="text-uppercase txt">online instructors</div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -15,4 +36,35 @@ export default {
 </script>
 
 <style lang="scss">
+@import "../assets/scss/variables.scss";
+
+#site_main {
+  section {
+    padding: 5rem 0;
+  }
+  .accomplishment {
+    position: relative;
+  }
+  .accomplishment::before {
+    content: url("../assets/img/background-pattern-grid-line-06.png");
+    position: absolute;
+    top: 0;
+  }
+  .accomplishment:last-child::after {
+    content: url("../assets/img/background-pattern-grid-line-06.png");
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  .numbers,
+  .txt {
+    font-weight: 800;
+    text-align: center;
+  }
+  .numbers {
+    font-size: 1.8rem;
+    margin-bottom: 0.5rem;
+    color: $brand_primary;
+  }
+}
 </style>
