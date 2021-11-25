ff39f660e9910a223ef14fd3e03c8289178a9a77
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-25 12:28:50 +0100

    start site header | add navbar template
    removed Vue default png logo | lost 1h after trying to install @popper/core required for bootstrap js import

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,8 +1,156 @@
 <template>
-  <div id="site_header"></div>
+  <div id="site_header">
+    <div class="container">
+      <!-- NAVBAR -->
+      <nav class="row" id="navbar">
+        <!-- LOGO -->
+        <div class="col-4 logo">
+          <img src="../assets/img/dark-logo.png" alt="MaxCoach logo" />
+        </div>
+
+        <!-- DROPDOWN LINKS -->
+        <ul class="col-4">
+          <li class="dropdown show">
+            <a
+              class="dropdown-toggle"
+              href="#"
+              role="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Home
+            </a>
+            <!-- Dropdown menu (NOT WORKING. MUST IMPORT BOOTSTRAP JS) -->
+            <!-- <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div> -->
+          </li>
+          <li class="dropdown show">
+            <a
+              class="dropdown-toggle"
+              href="#"
+              role="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Pages
+            </a>
+            <!-- Dropdown menu (NOT WORKING. MUST IMPORT BOOTSTRAP JS) -->
+            <!-- <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div> -->
+          </li>
+          <li class="dropdown show">
+            <a
+              class="dropdown-toggle"
+              href="#"
+              role="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Courses
+            </a>
+            <!-- Dropdown menu (NOT WORKING. MUST IMPORT BOOTSTRAP JS) -->
+            <!-- <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div> -->
+          </li>
+          <li class="dropdown show">
+            <a
+              class="dropdown-toggle"
+              href="#"
+              role="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Features
+            </a>
+            <!-- Dropdown menu (NOT WORKING. MUST IMPORT BOOTSTRAP JS) -->
+            <!-- <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div> -->
+          </li>
+          <li class="dropdown show">
+            <a
+              class="dropdown-toggle"
+              href="#"
+              role="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Blog
+            </a>
+            <!-- Dropdown menu (NOT WORKING. MUST IMPORT BOOTSTRAP JS) -->
+            <!-- <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div> -->
+          </li>
+          <li class="dropdown show">
+            <a
+              class="dropdown-toggle"
+              href="#"
+              role="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Shop
+            </a>
+            <!-- Dropdown menu (NOT WORKING. MUST IMPORT BOOTSTRAP JS) -->
+            <!-- <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div> -->
+          </li>
+        </ul>
+
+        <!-- SOCIALS  -->
+        <div class="col-4">
+          <ul class="socials">
+            <li>
+              <i class="fab fa-twitter"></i>
+            </li>
+            <li>
+              <i class="fas fa-facebook"></i>
+            </li>
+            <li>
+              <i class="fas fa-instagram"></i>
+            </li>
+            <li>
+              <i class="fas fa-linkedin-in"></i>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
 </template>
 
 <script>
+// import "bootstrap/js/dist/dropdown";
+
 export default {
   props: {},
 
@@ -15,4 +163,5 @@ export default {
 </script>
 
 <style lang="scss">
+@import "../assets/scss/variables.scss";
 </style>
