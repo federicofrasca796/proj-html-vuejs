1d46dcfdc48a3c7cc42d7e1fb636ed64977f7574
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-27 14:46:19 +0100

    add sect4 clients | fix markers in ul

--- src/components/site-main-components/sectionBooks.vue
+++ src/components/site-main-components/sectionBooks.vue
@@ -6,10 +6,10 @@
         <div class="section_title">Book Store Online</div>
         <h2>Be Alpha With <span class="primary_color">Wingman's Book</span></h2>
         <ul class="check_list">
-          <li>Help you understand yourself better</li>
-          <li>Revealing mature tips</li>
-          <li>Give the right advice</li>
-          <li>Fascinating examples of alpha man</li>
+          <li><span>Help you understand yourself better</span></li>
+          <li><span>Revealing mature tips</span></li>
+          <li><span>Give the right advice</span></li>
+          <li><span>Fascinating examples of alpha man</span></li>
         </ul>
 
         <a href="" class="btn btn_primary text-capitalize">Get free ebook</a>
@@ -18,7 +18,7 @@
       <!-- Books column -->
       <div class="col-8 ms-auto">
         <div class="row">
-          <div class="col-6" v-for="book in booksArr" :key="book.id">
+          <div class="col-6 gx-5" v-for="book in booksArr" :key="book.id">
             <div class="card">
               <img
                 class="card-img-top"
@@ -32,9 +32,9 @@
 
                 <!-- Course price -->
                 <div class="price primary_color">
-                  ${{ book.price[0] }}
-                  <span>
-                    .{{
+                  ${{ book.price[0]
+                  }}<span
+                    >.{{
                       book.price[1] === 0 ? book.price[1] + "0" : book.price[1]
                     }}
                   </span>
@@ -43,6 +43,29 @@
             </div>
           </div>
         </div>
+      </div>
+    </div>
+    <div class="row books_clients">
+      <!-- <div class="col-2">
+        <img src="@/assets/img/client-logo-01-primary.png" alt="client logo" />
+      </div> -->
+      <div class="col-2 d-flex align-items-center justify-content-center">
+        <img src="@/assets/img/client-logo-05-primary.png" alt="client logo" />
+      </div>
+      <div class="col-2 d-flex align-items-center justify-content-center">
+        <img src="@/assets/img/client-logo-06-primary.png" alt="client logo" />
+      </div>
+      <div class="col-2 d-flex align-items-center justify-content-center">
+        <img src="@/assets/img/client-logo-07-primary.png" alt="client logo" />
+      </div>
+      <div class="col-2 d-flex align-items-center justify-content-center">
+        <img src="@/assets/img/client-logo-04-primary.png" alt="client logo" />
+      </div>
+      <div class="col-2 d-flex align-items-center justify-content-center">
+        <img src="@/assets/img/client-logo-03-primary.png" alt="client logo" />
+      </div>
+      <div class="col-2 d-flex align-items-center justify-content-center">
+        <img src="@/assets/img/client-logo-02-primary.png" alt="client logo" />
       </div>
     </div>
   </section>
@@ -56,5 +79,12 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+.check_list {
+  margin: 2rem 0;
+}
+.books_clients {
+  margin: 8rem 0;
+  filter: grayscale(0.7) opacity(0.35);
+}
 </style>
