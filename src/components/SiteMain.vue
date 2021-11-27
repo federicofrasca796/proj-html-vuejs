a80782382c31ec1452d31c3b09e47f3780afbca3
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-27 12:11:38 +0100

    add section 4 w/ data struct | needs style tweak

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -78,6 +78,57 @@
         >View all courses <span class="img fs-6 ms-2">&#8594;</span></a
       >
     </section>
+
+    <!-- SECTION 4 "Book Store" -->
+    <section class="container">
+      <div class="row">
+        <div class="col-3">
+          <div class="section_title">Book Store Online</div>
+          <h2>
+            Be Alpha With <span class="primary_color">Wingman's Book</span>
+          </h2>
+          <ul class="check_list">
+            <li>Help you understand yourself better</li>
+            <li>Revealing mature tips</li>
+            <li>Give the right advice</li>
+            <li>Fascinating examples of alpha man</li>
+          </ul>
+
+          <a href="" class="btn btn_primary text-capitalize">Get free ebook</a>
+        </div>
+
+        <div class="col-6 ms-auto">
+          <div class="row">
+            <div class="col-6" v-for="book in books" :key="book.id">
+              <div class="card">
+                <img
+                  class="card-img-top"
+                  :src="require('../assets/img/' + book.thumb)"
+                  alt="book cover thumbnail"
+                />
+
+                <div class="card-title">
+                  <!-- Course title  -->
+                  <h3>{{ book.title }}</h3>
+
+                  <!-- Course price -->
+                  <div class="price primary_color">
+                    ${{ book.price[0]
+                    }}<span
+                      >.{{
+                        book.price[1] === 0
+                          ? book.price[1] + "0"
+                          : book.price[1]
+                      }}</span
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -140,6 +191,18 @@ export default {
           title: "Management Skills: The Science of Leadership",
           lessons: 17,
           students: 50,
+        },
+      ],
+      books: [
+        {
+          thumb: "product-book-11-400x400.jpg",
+          title: "Alpha man by Maxcoach",
+          price: [29, 0],
+        },
+        {
+          thumb: "product-book-10-400x400.jpg",
+          title: "Real man 4.0 by Maxcoach",
+          price: [39, 0],
         },
       ],
     };
