4c92db4c02f25317d43891fbab6276ddca2afcab
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-29 14:39:04 +0100

    move sect7 as comp

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -38,44 +38,7 @@
     <sectionStories :storiesArr="realStories" />
 
     <!-- SECTION 7 "Blog Articles" -->
-    <section class="container" id="blog_articles">
-      <!-- Section header  -->
-      <div class="row">
-        <h4 class="section_title">Blog Updated</h4>
-        <h2>
-          Interesting <span class="primary_color">articles updated</span> daily
-        </h2>
-      </div>
-
-      <!-- Articles cards -->
-      <div class="row">
-        <div class="col-3" v-for="article in articles" :key="article.key">
-          <div class="card">
-            <img
-              :src="require(`@/assets/img/${article.thumb}`)"
-              alt="article thumbnail"
-              class="card-img-top rounded-3"
-            />
-            <div class="card-title">
-              <div class="publish_date">
-                <i class="far fa-calendar"></i>
-                {{ article.publish_date }}
-              </div>
-
-              <h3>{{ article.title }}</h3>
-            </div>
-            <div class="card-text">
-              {{ article.text }}
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- View all CTA  -->
-      <div class="text-center my-5">
-        <a class="btn_underline_primary">View All Posts</a>
-      </div>
-    </section>
+    <sectionBlog :articlesArr="articles" />
   </main>
 </template>
 
@@ -85,6 +48,7 @@ import sectionCourses from "@/components/site-main-components/sectionCourses.vue
 import sectionBooks from "@/components/site-main-components/sectionBooks.vue";
 import sectionClients from "@/components/site-main-components/sectionClients.vue";
 import sectionStories from "@/components/site-main-components/sectionStories.vue";
+import sectionBlog from "@/components/site-main-components/sectionBlog.vue";
 
 export default {
   components: {
@@ -93,6 +57,7 @@ export default {
     sectionBooks,
     sectionClients,
     sectionStories,
+    sectionBlog,
   },
   props: {},
 
---
+++ src/components/site-main-components/sectionBlog.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="container" id="blog_articles">
+    <!-- Section header  -->
+    <div class="row">
+      <h4 class="section_title">Blog Updated</h4>
+      <h2>
+        Interesting <span class="primary_color">articles updated</span> daily
+      </h2>
+    </div>
+
+    <!-- Articles cards -->
+    <div class="row">
+      <div class="col-3" v-for="article in articlesArr" :key="article.key">
+        <div class="card">
+          <img
+            :src="require(`@/assets/img/${article.thumb}`)"
+            alt="article thumbnail"
+            class="card-img-top rounded-3"
+          />
+          <div class="card-title">
+            <div class="publish_date">
+              <i class="far fa-calendar"></i>
+              {{ article.publish_date }}
+            </div>
+
+            <h3>{{ article.title }}</h3>
+          </div>
+          <div class="card-text">
+            {{ article.text }}
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- View all CTA  -->
+    <div class="text-center my-5">
+      <a class="btn_underline_primary">View All Posts</a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    articlesArr: Array,
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
