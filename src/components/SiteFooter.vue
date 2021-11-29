5a7ddc17828c81cfa5e3a033920d15dfc55b6314
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-11-29 15:17:25 +0100

    start site footer | add data struct

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,5 +1,15 @@
 <template>
-  <div id="site_footer"></div>
+  <div id="site_footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-6"></div>
+        <div class="col-6 d-flex">
+          <div class="col-6"></div>
+          <div class="col-6"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,7 +17,60 @@ export default {
   props: {},
 
   data() {
-    return {};
+    return {
+      footer_sections: [
+        {
+          title: "Explore",
+          links: [
+            {
+              link_name: "Start here",
+              href: "#",
+            },
+            {
+              link_name: "Blog",
+              href: "#",
+            },
+            {
+              link_name: "About us",
+              href: "#",
+            },
+            {
+              link_name: "Success story",
+              href: "#",
+            },
+            {
+              link_name: "Courses",
+              href: "#",
+            },
+            {
+              link_name: "Contact us",
+              href: "#",
+            },
+          ],
+        },
+        {
+          title: "Information",
+          links: [
+            {
+              link_name: "Membership",
+              href: "#",
+            },
+            {
+              link_name: "Purchase guide",
+              href: "#",
+            },
+            {
+              link_name: "Privacy policy",
+              href: "#",
+            },
+            {
+              link_name: "Terms of service",
+              href: "#",
+            },
+          ],
+        },
+      ],
+    };
   },
 
   methods: {},
@@ -15,4 +78,10 @@ export default {
 </script>
 
 <style lang="scss">
+@import "../assets/scss/variables.scss";
+
+#site_footer {
+  background: $bg_dark;
+  color: $lighter_grey;
+}
 </style>
